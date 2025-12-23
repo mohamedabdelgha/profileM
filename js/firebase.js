f07e1ce9e23e80cb241e83394f25fbb1b3859e11
Mohamed Abd Elghany <89308535+mohamedabdelgha@users.noreply.github.com>
2025-12-23 20:55:58 +0200

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -68,7 +68,7 @@
           <p class="discreprion">${doc.data().discripe}</p>
           <span class="date"> ${formattedDate}</span>
           <div class="shadow">
-            <a meta-id="${doc.id}"><i class="fa-solid fa-link"></i> see more</a>
+            // <a meta-id="${doc.id}"><i class="fa-solid fa-link"></i> see more</a>
           </div>
           </div>
           `;
@@ -304,4 +304,5 @@ countDocuments(docref3).then((count) => {
           clearInterval(counter);
       }
   },duration);
+
 });
